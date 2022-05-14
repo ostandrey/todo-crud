26a67b08dac3b2888891b7318ff8c491ee4c1465
andrii-ostapov <ost.and.g@gmail.com>
2022-05-14 23:21:58 +0300

    Finish adding the logic

--- scripts/main.js
+++ scripts/main.js
@@ -26,7 +26,7 @@ let formValidation = () => {
     }
 }
 
-let data = []
+let data = [{}]
 
 let acceptData = () => {
     data.push({
@@ -41,25 +41,29 @@ let acceptData = () => {
 }
 
 let createData = () => {
-    data.map()
-    tasks.innerHTML += `
-    <div>
-      <span class="fw-bold">${data.text}</span>
-      <span class="small text-secondary">${data.date}</span>
-      <p>${data.description}</p>
-      <span class="options">
-          <i onclick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
-          <i onclick="deleteTask(this)" class="fas fa-trash-alt"></i>
-      </span>
-    </div>
-    `
-
+    tasks.innerHTML = '';
+    data.map((x, y) => {
+        return (
+            tasks.innerHTML += `
+            <div id=${y}>
+              <span class="fw-bold">${x.text}</span>
+              <span class="small text-secondary">${x.date}</span>
+              <p>${x.description}</p>
+              <span class="options">
+                  <i onclick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+                  <i onclick="deleteTask(this)" class="fas fa-trash-alt"></i>
+              </span>
+            </div>
+            `
+        )
+    })
     resetForm();
-
 }
 
 let deleteTask = (e) => {
     e.parentElement.parentElement.remove();
+    data.splice(+e.parentElement.parentElement.id, 1);
+    localStorage.setItem('data', JSON.stringify(data));
 }
 
 let editTask = (e) => {
@@ -69,7 +73,7 @@ let editTask = (e) => {
     dateInput.value = selectedTask.children[1].innerHTML;
     textarea.value = selectedTask.children[2].innerHTML;
 
-    selectedTask.remove();
+    deleteTask(e)
 }
 
 let resetForm = () => {
@@ -79,5 +83,6 @@ let resetForm = () => {
 }
 
 (() => {
-    data =localStorage.getItem('data')
+    data = JSON.parse(localStorage.getItem('data')) || []
+    createData()
 })()
